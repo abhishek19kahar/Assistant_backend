c5f47c5722c83257ab03608b16f5ba843d405a4d
abhishek19kahar <abhishekgithub19@gmail.com>
2025-07-28 13:45:10 +0530

    adding vercel json  backend on git

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "https://virtualassistant-drab.vercel.app", 
   credentials: true               
 }));
 
